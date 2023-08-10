11ea1302cdbebcf77ab436f16684b6aefe3108ff
ludovicMAK <ludovic93mak@gmail.com>
2023-08-10 16:26:36 +0200

    installation de validator et mise en place un model email et password required

--- app.js
+++ app.js
@@ -74,26 +74,52 @@ require('dotenv').config();
 //     return 'done'
 // }
 const mongoose = require('mongoose');
+const validator = require('validator')
 main().catch(err => console.log(err))
     
 
 async function main(){
     await mongoose.connect(process.env.MONGO_URL);
     const User = mongoose.model('User',{
-        name: String,
-        age:Number
+        // name: {
+        //     type:String,
+        //     required:true
+        // },
+        // age:{
+        //     type:Number,
+        //     required:true,
+        //     validate(c){
+        //         if(v<0) throw new Error('Age doit être positif!')
+        //     }
+        // }
+        email:{
+            type:String,
+            required:true,
+            validate(v){
+                if(!validator.isEmail(v)) throw new Error('Email non valide!');
+            }
+        },
+        password:{
+            type:String,
+            required:true,
+            validate(v){
+                if(!validator.isLength(v,{min:4,max:20})) throw new Error('le mdp doit être entre 4 à 20 caractère');
+            }
+        }
     });
 
     const firstPerson = new User({
-        name:'alex',
-        age:30
+       email:'test@exemple.com',
+       password:'password'
     });
     const secondPerson = new User({
-        name:'Pauline',
-        age:30
+        email:'Justine@gmail.com',
+        password:'password'
     });
 
-    console.log(firstPerson,secondPerson);
-    await firstPerson.save();
-    await secondPerson.save();
+    
+    const firstSave = await firstPerson.save();
+    const secondeSave = await secondPerson.save();
+
+    console.log(firstSave,secondeSave);
 }
