67f5eb730dab2486bf39dacbb23cd27a72e516f3
ludovicMAK <ludovic93mak@gmail.com>
2023-08-10 15:27:14 +0200

    Mise en place du CRUD

--- app.js
+++ app.js
@@ -5,11 +5,72 @@ const client = new MongoClient(process.env.MONGO_URL)
 
 async function main(){
     await client.connect();
-    
+    console.log("connextion OK")
     const db = client.db('myTask');
     const collection = db.collection('documents');
-    const insertStuff = await collection.insertMany([{a:1},{b:2},{c:3},{d:4}]);
-    console.log(`Documents insérés => `, insertStuff);
+
+    //Create
+    // try{
+    //     const inserData = await collection.insertMany([
+    //         {
+    //             name:'Alex',
+    //             age:30,
+    //             sexe:'masculin',
+    //             hobby:'coding'
+    //         },
+    //         {
+    //             name:'Justine',
+    //             age:30,
+    //             sexe:'Féminin',
+    //             hobby:'coding'
+    //         },
+    //         {
+    //             name:'Pierre',
+    //             age:35,
+    //             sexe:'masculin',
+    //             hobby:'escalade'
+    //         }
+    //     ]);
+
+    //     console.log('Document insérés =>',inserData);
+    // } catch(e){
+    //     throw e;
+    // }
+
+    //Read
+    try{
+        // const findData = await collection.findOne({name:'Justine'});
+        // console.log('Document trouvé:',findData);
+
+        // const findMultipleData = await collection.find({age:30});
+        // console.log('Document trouvé:', await findMultipleData.toArray());
+    } catch(e){
+        throw e;
+    }
+
+    //Update
+    // try{
+    //     const UpdateAge = collection.updateMany({age:30},{
+    //         $set:{age:31}
+    //     });
+        
+    //     console.log(await UpdateAge);
+    // } catch(e){
+    //     throw e;
+    // }
+
+    //delete
+    try{
+        const deletePierre = await collection.deleteOne({name:'Pauline'});
+        console.log(await deletePierre);
+
+        const deleteEveryone = await collection.deleteMany({age:31});
+        console.log(await deleteEveryone);
+    } catch(e){
+        throw e;
+    }
+    // const insertStuff = await collection.insertMany([{a:1},{b:2},{c:3},{d:4}]);
+    // console.log(`Documents insérés => `, insertStuff);
     return 'done'
 }
 
